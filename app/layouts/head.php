f9cbfb3758cbb592a521362f7403b695ad45dc5e
JennyViannay <jenny.viannay75@gmail.com>
2022-10-02 19:01:06 +0200

    fix some pb

--- app/layouts/head.php
+++ app/layouts/head.php
@@ -11,5 +11,5 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <!-- CSS only -->
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
 </head>
--- app/pages/_default.template.php
+++ app/pages/_default.template.php
@@ -5,10 +5,11 @@
  * URL: -
  */
 
-include_once 'layouts/head.php';
-include_once 'layouts/body_start.php';
-include_once 'layouts/container_start.php';
-require '../.connec.php';
+include_once '../../layouts/head.php';
+include_once '../../layouts/body_start.php';
+include_once '../../layouts/container_start.php';
+
+require '../../../.connec.php';
 ?>
 <div class="row">
     </div>
@@ -16,7 +17,7 @@ require '../.connec.php';
     // Your code here
     <!-- 📝 INSERER LE CODE HTML DE LA PAGE ICI -->
 <?php 
-include_once('layouts/container_end.php');
-include_once ('layouts/scripts.php');
-include_once('layouts/body_end.php');
+include_once('../../layouts/container_end.php');
+include_once('../../layouts/scripts.php');
+include_once('../../layouts/body_end.php');
 ?>
